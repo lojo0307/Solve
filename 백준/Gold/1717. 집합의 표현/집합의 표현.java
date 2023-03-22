8766e3112a3d3aac0eda828bf32098e4a31bf5b6
MyeongJu <122420081+JangMyeongJu@users.noreply.github.com>
2023-03-22 17:43:54 +0900

    [Gold V] Title: 집합의 표현, Time: 972 ms, Memory: 59532 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[] idx;
+	
+	static void union(int a, int b) {
+		a = find(a);
+		b = find(b);
+		if (a != b) idx[b] = a;
+	}
+	
+	static int find(int i) {
+		if (idx[i] == i) return i;
+		return idx[i] = find(idx[i]);
+	}
+	
+	static boolean match(int a, int b) {
+		a = find(a);
+		b = find(b);
+		if (a == b) return true;
+		return false;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		StringTokenizer token = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(token.nextToken());
+		int m = Integer.parseInt(token.nextToken());
+		
+		idx = new int[n+1];
+		for (int i = 0; i <= n; i++) {
+			idx[i] = i;
+		}
+		
+		for (int i = 0; i < m; i++) {
+			token = new StringTokenizer(br.readLine());
+			int cal = Integer.parseInt(token.nextToken());
+			int a = Integer.parseInt(token.nextToken());
+			int b = Integer.parseInt(token.nextToken());
+			
+			if (cal == 0) union(a, b);
+			else {
+				if (match(a, b)) sb.append("YES\n");
+				else sb.append("NO\n"); 
+			}
+		}
+		System.out.println(sb);
+	}
+}
