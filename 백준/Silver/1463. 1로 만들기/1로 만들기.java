cd6008a049ff30763d4146bffa79f64433a8bf29
MyeongJu <122420081+JangMyeongJu@users.noreply.github.com>
2023-03-28 14:14:03 +0900

    [Silver III] Title: 1로 만들기, Time: 256 ms, Memory: 76576 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,53 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
+	static Integer[] res;
 	
-	static Integer[] dp; // 메모이제이션 할 배열
-	 
-	static int recur(int N) {
-		if (N == 1) return 0;
-		if (dp[N] == null) {
-			// 6으로 나눠지는 경우 
-			if (N % 6 == 0) {
-				dp[N] = Math.min(recur(N - 1), Math.min(recur(N / 3), recur(N / 2))) + 1;
-			}
-			// 3으로만 나눠지는 경우 
-			else if (N % 3 == 0) {
-				dp[N] = Math.min(recur(N / 3), recur(N - 1)) + 1;
-			}
-			// 2로만 나눠지는 경우 
-			else if (N % 2 == 0) {
-				dp[N] = Math.min(recur(N / 2), recur(N - 1)) + 1;
-			}
-			// 2와 3으로 나누어지지 않는 경우
-			else {
-				dp[N] = recur(N - 1) + 1;
-			}
+	static int cal(int a) {
+		if (a == 1) return 0;
+		if (res[a] == null) {
+	//		res[a] = cal(a-1);
+			if (a%6 == 0) res[a] = Math.min(cal(a-1), Math.min(cal(a/2), cal(a/3)))+1;
+			else if (a%3 == 0) res[a] = Math.min(cal(a/3), cal(a-1))+1;
+			//a/3
+			else if (a%2 == 0) res[a] = Math.min(cal(a/2), cal(a-1))+1;
+			else res[a] = cal(a-1)+1;
 		}
-		return dp[N];
+		return res[a];
 	}
-//	static int[] res;
-//	
-//	static int cal(int a) {
-//		if (res[a] != 0) return res[a];
-//		if (a == 1) return 0;
-//		//a-1
-//		int temp = cal(a-1);
-//		//a/2
-//		if (a%2 == 0) {
-//			temp = Math.min(temp, cal(a/2));
-//		}
-//		//a/3
-//		if (a%3 == 0) {
-//			temp = Math.min(temp, cal(a/3));
-//		}
-//		return res[a] = temp + 1;
-//	}
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		dp = new Integer[n+1];
-		System.out.println(recur(n));
+		res = new Integer[n+1];
+		System.out.println(cal(n));
 	}
 }
