c3126ecac6c761c1e24a97b019dfd81a811276bd
MyeongJu <122420081+JangMyeongJu@users.noreply.github.com>
2023-03-30 17:59:09 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 116 ms, Memory: 13576 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,20 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		BigInteger[] dp = new BigInteger[n+2];
+		dp[1] = BigInteger.ONE;
+		dp[2] = new BigInteger("3");
+		
+		for (int i = 3; i < dp.length; i++) {
+			dp[i] = dp[i-1].add(dp[i-2].add(dp[i-2]));
+		}
+		
+		System.out.println(dp[n].mod(new BigInteger("10007")));
+	}
+}
