6f95625eca4327e256c28dd5334a123b02d25626
lojo0307 <122420081+lojo0307@users.noreply.github.com>
2024-01-31 02:00:47 +0900

    [level 3] Title: 야근 지수, Time: 1.90 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -2,36 +2,20 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] works) {
-        Arrays.sort(works);
-        
-        int[] interval = new int[works.length];
-        for (int i = interval.length-1; i > 0; i--) {
-            interval[i] = works[i] - works[i-1];
-        }
-        interval[0] = works[0];
-        
-        for (int i = interval.length - 1; i >= 0; i--) {
-            int size = interval.length - i;
-            if (interval[i] * size <= n) {
-                if (i == 0) return 0;
-                n -= (interval[i] * size);
-                continue;
-            }
-            while (n >= size) {
-                n -= size;
-                interval[i]--;
-            }
-            for (int k = i; k < works.length; k++) {
-                if (n-- > 0) works[k] = (i > 0 ? works[i-1] : 0) + interval[i] - 1;
-                else works[k] = (i > 0 ? works[i-1] : 0) + interval[i];
-            }
-            break;
-        }
-        
-        long sum = 0;
+        Queue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
         for (int work : works) {
-            sum += Math.pow(work, 2);
+            pq.offer(work);
         }
-        return sum;
+        
+        while (n-- > 0 && !pq.isEmpty()) {
+            int num = pq.poll();
+            if (num != 1) pq.offer(num - 1);
+        }
+        
+        int answer = 0;
+        while (!pq.isEmpty()) {
+            answer += Math.pow(pq.poll(), 2);
+        }
+        return answer;
     }
 }
