b45711d559fe58c8ebefcb8c994b8756af4ecaeb
lojo0307 <122420081+lojo0307@users.noreply.github.com>
2024-02-20 09:32:51 +0900

    [level 4] Title: 도둑질, Time: 21.41 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -1,23 +1,15 @@
 class Solution {
     public int solution(int[] money) {
-        int[] dp = new int[money.length];
+        int[][] dp = new int[2][money.length];
         
-        // 0번 털 경우
-        dp[0] = money[0];
-        dp[1] = money[0];
-        for (int i = 2; i < money.length-1; i++) {
-            dp[i] = Math.max(dp[i-2] + money[i], dp[i-1]);
-        }
-        
-        int res1 = dp[dp.length-2];
-        
-        // 0번 방문 X
-        dp[0] = 0;
-        dp[1] = money[1];
+        dp[0][0] = money[0];
+        dp[0][1] = money[0];
+        dp[1][1] = money[1];
         for (int i = 2; i < money.length; i++) {
-            dp[i] = Math.max(dp[i-2] + money[i], dp[i-1]);
+            dp[0][i] = Math.max(dp[0][i-2] + money[i], dp[0][i-1]);
+            dp[1][i] = Math.max(dp[1][i-2] + money[i], dp[1][i-1]);
         }
         
-        return Math.max(res1, dp[dp.length-1]);
+        return Math.max(dp[0][dp[0].length-2], dp[1][dp[1].length-1]);
     }
 }
