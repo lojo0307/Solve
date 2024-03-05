6057fa33475f7df3b5d48e3d407277f828478383
lojo0307 <122420081+lojo0307@users.noreply.github.com>
2024-03-05 13:21:39 +0900

    [level 3] Title: 표 편집, Time: 211.52 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -2,59 +2,52 @@ import java.util.*;
 
 class Solution {
     public String solution(int n, int k, String[] cmd) {
-        Node head = new Node(-1);
-        Node cur = head;
-        for (int i = 0; i < n; i++) {
-            Node node = new Node(i);
-            cur.next = node;
-            node.prev = cur;
-            cur = node;
-        }
-        while (cur.idx != k) {
-            cur = cur.prev;
-        }
-        Stack<Node> stack = new Stack();
+        boolean[] r = new boolean[n];
+        Stack<Integer> stack = new Stack();
+        int idx = k;
         
-        for (int i = 0; i < cmd.length; i++) {
+        for(int i = 0; i < cmd.length; i++) {
             switch (cmd[i].charAt(0)) {
-                case 'U':
+                case 'U' : // up
                     int cnt = Integer.parseInt(cmd[i].substring(2));
-                    while (cnt-- > 0) cur = cur.prev;
+                    while (cnt-- > 0) {
+                        if (r[--idx]) cnt++;
+                    }
                     break;
-                case 'D': 
+                case 'D' : // down
                     int cnt1 = Integer.parseInt(cmd[i].substring(2));
-                    while (cnt1-- > 0) cur = cur.next;
+                    while (cnt1-- > 0) {
+                        if (r[++idx]) cnt1++;
+                    }
                     break;
-                case 'C': 
-                    stack.add(cur);
-                    cur.prev.next = cur.next;
-                    if (cur.next != null) cur.next.prev = cur.prev;
-                    cur = cur.next != null ? cur.next : cur.prev;
+                case 'C' : // del + down
+                    r[idx] = true;
+                    stack.add(idx);
+                    int temp = idx - 1;
+                    if (idx + 1 == n) {
+                        while (r[idx]) {idx--;}
+                    } else {
+                        loop : while (r[++idx]) {
+                        if (idx == n - 1) {
+                            idx = temp;
+                            while (r[idx]) {idx--;}
+                            break loop;
+                            }
+                        }
+                    }
+                    
                     break;
-                case 'Z':
-                    Node temp = stack.pop();
-                    temp.prev.next = temp;
-                    if (temp.next != null) temp.next.prev = temp;
-                    break;
+                case 'Z' : // restore
+                    r[stack.pop()] = false;
             }
         }
         
-        
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) sb.append("O");
-        while (!stack.isEmpty()) {
-            sb.setCharAt(stack.pop().idx ,'X');
+        for (int i = 0; i < n; i++) {
+            if (!r[i]) sb.append("O");
+            else sb.append("X");
         }
         
         return sb.toString();
-    }
-}
-
-class Node {
-    int idx;
-    Node prev;
-    Node next;
-    public Node(int idx) {
-        this.idx = idx;
     }
 }
