0ea1e37113d94b08aacb01fc535e0f0d53d917e4
lojo0307 <122420081+lojo0307@users.noreply.github.com>
2024-03-09 01:32:07 +0900

    [level 3] Title: 보행자 천국, Time: 142.48 ms, Memory: 99.7 MB -BaekjoonHub

--- 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
+++ 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
@@ -1,36 +1,31 @@
 class Solution {
     int MOD = 20170805;
     public int solution(int m, int n, int[][] cityMap) {
-        if (m == 1 && n == 1) return 1;
+        map = new int[cityMap.length][cityMap[0].length];
+        for (int r = 0; r < map.length; r++) {
+            for (int c = 0; c < map[0].length; c++) map[r][c] = cityMap[r][c];
+        }
         
-        int[][][] dp = new int[m][n][2];
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                if (r == 0 && c == 0) continue;
-                if (r == 1 && c == 0) {
-                    dp[r][c][1] = 1;
-                    continue;
-                }
-                if (r == 0 && c == 1) {
-                    dp[r][c][0] = 1;
-                    continue;
-                }
-                
-                if (cityMap[r][c] == 1) continue;
-                
-                if (c > 0) {
-                    if (cityMap[r][c-1] == 0) dp[r][c][0] += dp[r][c-1][1];
-                    dp[r][c][0] += dp[r][c-1][0];
-                    dp[r][c][0] %= MOD;
-                }
-                if (r > 0) {
-                    if (cityMap[r-1][c] == 0) dp[r][c][1] += dp[r-1][c][0];
-                    dp[r][c][1] += dp[r-1][c][1];
-                    dp[r][c][1] %= MOD;
-                }
+        return drive(0, 0, true);
+    }
+    
+    static int[][] map;
+    public int drive(int r, int c, boolean dir) { // true면 왼->오 false면 위->아래
+        if (r == map.length - 1 && c == map[0].length - 1) return 1;
+        if (map[r][c] == 1) return 0;
+        if (map[r][c] == 2) {
+            if (dir) {
+                if (c + 1 == map[0].length) return 0;
+                return drive(r, c+1, true);
+            } else {
+                if (r + 1 == map.length) return 0;
+                return drive(r+1, c, false);
             }
         }
         
-        return (dp[m-1][n-1][0] + dp[m-1][n-1][1]) % MOD;
+        int temp = 0;
+        if (r != map.length - 1) temp += drive(r+1, c, false);
+        if (c != map[0].length - 1) temp += drive(r, c+1, true);
+        return temp % MOD;
     }
 }
