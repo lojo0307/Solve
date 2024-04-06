368cde309a9fcb90ea38c2707697864b2c956560
lojo0307 <122420081+lojo0307@users.noreply.github.com>
2024-04-06 21:30:23 +0900

    [level 2] Title: 올바른 괄호, Time: 6.37 ms, Memory: 52.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,13 @@
+class Solution {
+    boolean solution(String s) {
+        
+        int cnt = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') cnt++;
+            else if (--cnt < 0) return false;
+        }
+        if (cnt == 0) return true;
+        return false;
+    }
+}
