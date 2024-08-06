f8fceab7be30cdeae52f443e49fc2ae48998c9d3
lojo0307 <122420081+lojo0307@users.noreply.github.com>
2024-08-06 10:12:23 +0900

    [level 3] Title: 최고의 집합, Time: 0.24 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -1,19 +1,12 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int n, int s) {
-        if (n > s) return new int[] {-1};
+        if (s < n) return new int[] {-1};
         int a = s / n;
-        int b = s - a * n;
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = a;
-            if (b > 0) {
-                arr[i]++;
-                b--;
-            }
-        }
-        Arrays.sort(arr);
-        return arr;
+        int b = s % n;
+        int[] ans = new int[n];
+        for (int i = 0; i < n-b; i++) ans[i] = a;
+        for (int i = n-b; i < n; i++) ans[i] = a+1;
+        return ans;
+        
     }
 }
