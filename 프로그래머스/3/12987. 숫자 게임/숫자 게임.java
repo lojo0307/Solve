c607ef4df544c7862f686a181bff6af4aa3fc836
lojo0307 <122420081+lojo0307@users.noreply.github.com>
2024-08-06 09:08:16 +0900

    [level 3] Title: 숫자 게임, Time: 59.91 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -1,21 +1,18 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] A, int[] B) {
         Arrays.sort(A);
         Arrays.sort(B);
         
-        int a = A.length - 1;
-        int b = B.length - 1;
-        int cnt = 0;
-        
-        while (a >= 0 && b >= 0) {
-            if (A[a] < B[b]) {
-                cnt++;
-                b--;
-            }
-            a--;
+        int res = 0;
+        int b = 0;
+        loop : for (int a = 0; a < A.length; a++) {
+            while (A[a] >= B[b]) if (++b == B.length) break loop;
+            res++;
+            if (++b == B.length) break loop;
         }
         
-        return cnt;
+        return res;
     }
 }
