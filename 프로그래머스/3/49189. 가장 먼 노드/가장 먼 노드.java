a53014d0909dde9f91c10278a753c7d0fa8ee8e5
lojo0307 <122420081+lojo0307@users.noreply.github.com>
2024-08-14 10:14:42 +0900

    [level 3] Title: 가장 먼 노드, Time: 55.30 ms, Memory: 98 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,40 +2,32 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] edge) {
-        List<Integer>[] node = new ArrayList[n+1];
-        for (int i = 1; i < n+1; i++) {
-            node[i] = new ArrayList<>();
-        }
-        for (int[] arr : edge) {
-            node[arr[0]].add(arr[1]);
-            node[arr[1]].add(arr[0]);
+        List<Integer>[] list = new LinkedList[n+1];
+        for (int i = 1; i <= n; i++) list[i] = new LinkedList<>();
+        for (int[] e : edge) {
+            list[e[0]].add(e[1]);
+            list[e[1]].add(e[0]);
         }
         
         boolean[] visited = new boolean[n+1];
-        int[] dist = new int[n+1];
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {1, 0});
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+        pq.offer(new int[] {1, 0}); // 노드 번호, 거리
         visited[1] = true;
-        while (!q.isEmpty()) {
-            int cur = q.peek()[0];
-            dist[cur] = q.poll()[1];
-            
-            for (int next : node[cur]) {
+        int[] dist = new int[n+1];
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            for (int next : list[cur[0]]) {
                 if (visited[next]) continue;
                 visited[next] = true;
-                
-                q.offer(new int[] {next, dist[cur] + 1});
+                dist[next] = cur[1] + 1;
+                pq.offer(new int[] {next, dist[next]});
             }
         }
         
-        int max = -1;
-        for (int val : dist) {
-            max = Math.max(val, max);
-        }
+        int max = 0;
+        for (int d : dist) max = Math.max(d, max);
         int cnt = 0;
-        for (int val : dist) {
-            if (val == max) cnt++;
-        }
+        for (int d : dist) if (d == max) cnt++;
         
         return cnt;
     }
