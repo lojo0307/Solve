a1e7cfd2ff7194b9b16d13c9f1fd5cb2cd1b74d6
lojo0307 <122420081+lojo0307@users.noreply.github.com>
2024-08-16 10:56:04 +0900

    [level 3] Title: 여행경로, Time: 2.12 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,53 +1,51 @@
 import java.util.*;
 
 class Solution {
-    static void dfs(String start, int i) {
-        if (i == num + 1) {
+    public String[] solution(String[][] tickets) {
+        map = new HashMap<>();
+        n = tickets.length + 1;
+        for(int i = 0; i < tickets.length; i++) {
+            if (!map.containsKey(tickets[i][0])) map.put(tickets[i][0], new LinkedList<>());
+            map.get(tickets[i][0]).add(new Node(tickets[i][1], i));
+        }
+        
+        visited = new boolean[tickets.length];
+        map.forEach((key, value) -> value.sort((o1, o2) -> o1.code.compareTo(o2.code)));
+        
+        ans = new String[n];
+        ans[0] = "ICN";
+        dfs(1);
+        return ans;
+    }
+    
+    static String[] ans;
+    static int n;
+    static Map<String, List<Node>> map;
+    static boolean[] visited;
+    static boolean flag;
+    
+    public void dfs(int idx) {
+        if (idx == n) {
             flag = true;
             return;
         }
-        
-        List<Next> list = map.getOrDefault(start, new ArrayList<>());
-        for (Next n : list) {
-            if (visited[n.idx]) continue;
-            visited[n.idx] = true;
-            ans[i] = n.arr;
-            dfs(n.arr, i + 1);
+        if (!map.containsKey(ans[idx-1])) return;
+        for (Node next : map.get(ans[idx-1])) {
+            if (visited[next.idx]) continue;
+            visited[next.idx] = true;
+            ans[idx] = next.code;
+            dfs(idx+1);
             if (flag) return;
-            visited[n.idx] = false;
+            visited[next.idx] = false;
         }
-    }
-    
-    static Map<String, List<Next>> map;
-    static boolean[] visited;
-    static String[] ans;
-    static int num;
-    static boolean flag;
-    
-    public String[] solution(String[][] tickets) {
-        num = tickets.length;
-        ans = new String[tickets.length + 1];
-        map = new HashMap<>();
-        for (int i = 0; i < tickets.length; i++) {
-            if (!map.containsKey(tickets[i][0])) map.put(tickets[i][0], new ArrayList<>());
-            map.get(tickets[i][0]).add(new Next(tickets[i][1], i));
-        }
-        map.forEach((key, val) -> {
-            Collections.sort(val, (a, b) -> a.arr.compareTo(b.arr));
-        });
-        
-        visited = new boolean[tickets.length];
-        ans[0] = "ICN";
-        dfs("ICN", 1);
-        return ans;
     }
 }
 
-class Next {
-    String arr;
+class Node {
+    String code;
     int idx;
-    public Next(String arr, int idx) {
-        this.arr = arr;
+    public Node(String code, int idx) {
+        this.code = code;
         this.idx = idx;
     }
 }
