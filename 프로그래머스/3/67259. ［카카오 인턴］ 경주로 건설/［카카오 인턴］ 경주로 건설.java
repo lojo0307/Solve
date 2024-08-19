b4ebdd5b9274720673d9043fdf9c6bc87a6ca28e
lojo0307 <122420081+lojo0307@users.noreply.github.com>
2024-08-19 11:12:57 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 5.52 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -2,15 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] board) {
-        boolean[][] visited = new boolean[board.length][board[0].length];
-        Queue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
-        return 1;
+        int n = board.length;
+        int[] dr = {0, 1, 0, -1};
+        int[] dc = {1, 0, -1, 0};
+        boolean[][][] visited = new boolean[n][n][4];
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[3] - o2[3]); // 비용 순 정렬
+        pq.offer(new int[]{0, 0, 0, 0});
+        pq.offer(new int[]{0, 0, 1, 0}); // 0 오 1 하 2 좌 3 상
+        
+        while(!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            if (visited[cur[0]][cur[1]][cur[2]]) continue;
+            if (cur[0] == n - 1 && cur[1] == n - 1) return cur[3];
+            visited[cur[0]][cur[1]][cur[2]] = true;
+            
+            for (int i = 0; i < 4; i++) {
+                if (i == (cur[2] + 2) % 4) continue;
+                int nr = cur[0] + dr[i];
+                int nc = cur[1] + dc[i];
+                if (nr < 0 || nc < 0 || nr >= n || nc >= n || visited[nr][nc][i] || board[nr][nc] == 1) continue;
+                pq.offer(new int[] {nr, nc, i, cur[3] + (i == cur[2] ? 100 : 600)});
+            }
+        }
+        
+        return -1;
     }
-}
-
-class Node {
-    int r;
-    int c;
-    int cost;
-    int dist; // 0:우 1:하 2:좌 3:상
 }
