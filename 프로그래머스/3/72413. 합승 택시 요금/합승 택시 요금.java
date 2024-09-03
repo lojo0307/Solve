19e292713054ca9aa29a4d0e9c80832c94e05738
lojo0307 <122420081+lojo0307@users.noreply.github.com>
2024-09-03 11:16:35 +0900

    [level 3] Title: 합승 택시 요금, Time: 68.40 ms, Memory: 65.5 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,44 +1,62 @@
 import java.util.*;
 
 class Solution {
-    static List<int[]>[] edges;
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        edges = new ArrayList[n+1];
-        for (int i = 1; i <= n; i++) edges[i] = new ArrayList<>();
-        for (int[] fare : fares) {
-            edges[fare[0]].add(new int[] {fare[1], fare[2]});
-            edges[fare[1]].add(new int[] {fare[0], fare[2]});
+        List<int[]>[] list = new LinkedList[n+1];
+        for (int i = 1; i <= n; i++) list[i] = new LinkedList<>();
+        for (int[] f : fares) {
+            list[f[0]].add(new int[] {f[1], f[2]});
+            list[f[1]].add(new int[] {f[0], f[2]});
         }
         
-        int min = Integer.MAX_VALUE;
-        for (int i = 1; i <= n; i++) {
-            int d1 = dist(s, i);
-            int d2 = dist(i, a);
-            int d3 = dist(i, b);
-            
-            if (d1 < 0 || d2 < 0 || d3 < 0) continue;
-            min = Math.min(min, d1 + d2 + d3);
-        }
+        int[] fee = new int[n+1];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+        boolean[] visited = new boolean[n+1];
+        pq.offer(new int[] {s, 0});
         
-        return min;
-    }
-    
-    private int dist(int start, int end) {
-        Queue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-        boolean[] visited = new boolean[edges.length];
-        
-        pq.offer(new int[] {start, 0}); // 0: 노드, 1: 거리
         while (!pq.isEmpty()) {
             int[] cur = pq.poll();
             if (visited[cur[0]]) continue;
-            if (cur[0] == end) return cur[1];
             visited[cur[0]] = true;
-            
-            for (int[] next : edges[cur[0]]) {
+            fee[cur[0]] = cur[1];
+            for (int[] next : list[cur[0]]) {
                 if (visited[next[0]]) continue;
-                pq.offer(new int[] {next[0], next[1] + cur[1]});
+                pq.offer(new int[] {next[0], cur[1] + next[1]});
             }
         }
-        return -1;
+        
+        pq.offer(new int[] {a, 0});
+        visited = new boolean[n+1];
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            if (visited[cur[0]]) continue;
+            visited[cur[0]] = true;
+            fee[cur[0]] += cur[1];
+            for (int[] next : list[cur[0]]) {
+                if (visited[next[0]]) continue;
+                pq.offer(new int[] {next[0], cur[1] + next[1]});
+            }
+        }
+        
+        pq.offer(new int[] {b, 0});
+        visited = new boolean[n+1];
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            if (visited[cur[0]]) continue;
+            visited[cur[0]] = true;
+            fee[cur[0]] += cur[1];
+            for (int[] next : list[cur[0]]) {
+                if (visited[next[0]]) continue;
+                pq.offer(new int[] {next[0], cur[1] + next[1]});
+            }
+        }
+        
+        int min = Integer.MAX_VALUE;
+        for (int i : fee) {
+            if (i == 0) continue;
+            min = Math.min(min, i);
+        }
+        
+        return min;
     }
 }
