a66b421bec103ef76056f1a471ae49d922edc9be
lojo0307 <122420081+lojo0307@users.noreply.github.com>
2024-09-04 20:08:03 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 62.08 ms, Memory: 226 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,27 +1,37 @@
 class Solution {
     public int solution(int[][] board, int[][] skill) {
-        
-        int[][] degree = new int[board.length][board[0].length];
+        int[][] map = new int[board.length+1][board[0].length+1];
         for (int[] s : skill) {
-            int d = (s[0] == 1 ? -1 : 1);
-            d *= s[5];
-            degree[s[1]][s[2]] += d;
-            if (s[4] + 1 < degree[0].length) degree[s[1]][s[4]+1] -= d;
-            if (s[3] + 1 < degree.length) degree[s[3]+1][s[2]] -= d;
-            if (s[4] + 1 < degree[0].length && s[3] + 1 < degree.length) degree[s[3]+1][s[4]+1] += d;
+            switch (s[0]) {
+                case 1 : // 공격
+                    map[s[1]][s[2]] -= s[5];
+                    map[s[3]+1][s[2]] += s[5];
+                    map[s[1]][s[4]+1] += s[5];
+                    map[s[3]+1][s[4]+1] -= s[5];
+                    break;
+                case 2 : // 회복
+                    map[s[1]][s[2]] += s[5];
+                    map[s[3]+1][s[2]] -= s[5];
+                    map[s[1]][s[4]+1] -= s[5];
+                    map[s[3]+1][s[4]+1] += s[5];
+            }
         }
         
-        for (int r = 0; r < board.length; r++) {
-            for (int c = 1; c < board[0].length; c++) degree[r][c] += degree[r][c-1];
+        for (int c = 0; c < map[0].length; c++) {
+            for (int r = 1; r < map.length; r++) map[r][c] += map[r-1][c];
         }
-        for (int c = 0; c < board[0].length; c++) {
-            for (int r = 1; r < board.length; r++) degree[r][c] += degree[r-1][c];
+        
+        for (int r = 0; r < map.length; r++) {
+            for (int c = 1; c < map[0].length; c++) map[r][c] += map[r][c-1];
         }
         
         int ans = 0;
         for (int r = 0; r < board.length; r++) {
-            for (int c = 0; c < board[0].length; c++) if (board[r][c] + degree[r][c] > 0) ans++;
+            for (int c = 0; c < board[0].length; c++) {
+                if (map[r][c] + board[r][c] > 0) ans++;
+            }
         }
+        
         return ans;
     }
 }
